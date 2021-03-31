08fc908de86a3f273335bbd5fcc83eff12be70c2
wangsq <wangsq@tongtech.com>
2021-03-31 16:30:10 +0800

    update

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	tables, _ := ipt.New()
-	chains, _ := tables.ListChains(os.Args[1])
+	s := os.Args[1]
+	chains, _ := tables.ListChains(s)
 	for _, chain := range chains {
 		println(chain)
+		list, _ := tables.List(s, chain)
+		println(list)
 	}
 }
